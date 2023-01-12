ff2734942d7ae372bc22fe06c39d50aa54ee4e1f
Marek Guráň <skemgi2@gmail.com>
2023-01-12 19:40:03 +0100

    Hobbies changed to website

--- app/src/main/java/com/marekguran/webpage/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/marekguran/webpage/ui/dashboard/DashboardFragment.java
@@ -4,15 +4,46 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.marekguran.webpage.R;
 import com.marekguran.webpage.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
+    WebView webView;
+
+    public DashboardFragment() {
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        webView=(WebView)getActivity().findViewById(R.id.webview);
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("your_url");
+
+        webView.setWebViewClient(new Mybrowser());
+        webView.loadUrl("https://marek-guran.sk/");
+    }
+
+    private class Mybrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
 
     private FragmentDashboardBinding binding;
 
