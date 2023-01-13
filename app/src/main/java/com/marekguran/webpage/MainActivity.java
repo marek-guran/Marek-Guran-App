27b8dd67d97536820bf2775bd92861ca3c29601b
Marek Guráň <skemgi2@gmail.com>
2023-01-13 15:05:18 +0100

    Small fixes

--- app/src/main/java/com/marekguran/webpage/MainActivity.java
+++ app/src/main/java/com/marekguran/webpage/MainActivity.java
@@ -1,16 +1,16 @@
 package com.marekguran.webpage;
 
+import android.os.Build;
 import android.os.Bundle;
-
+import android.view.View;
+import android.view.WindowManager;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.WindowCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
 import com.marekguran.webpage.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
